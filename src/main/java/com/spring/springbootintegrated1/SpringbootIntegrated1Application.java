f92a87bf670f01238a089ec805d81da479837126
yanglin <893713339@qq.com>
2020-05-26 23:27:30 +0800

    SpringBoot initial commit

---
+++ src/main/java/com/spring/springbootintegrated1/SpringbootIntegrated1Application.java
@@ -0,0 +1,13 @@
+package com.spring.springbootintegrated1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrated1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIntegrated1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/springbootintegrated1/SpringbootIntegrated1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springbootintegrated1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntegrated1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
